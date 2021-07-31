afb08141084b8ba3a55a6af7cbc0ca35f00c0f5b
snchz29 <sasha4888.terehov@gmail.com>
2021-07-31 16:15:32 +0300

    add abstract class for FriendshipGraph

--- src/main/java/ru/snchz29/Application.java
+++ src/main/java/ru/snchz29/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/ru/snchz29/services/FriendshipGraphImpl.java
@@ -0,0 +1,72 @@
+package ru.snchz29.services;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultimap;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import lombok.SneakyThrows;
+import ru.snchz29.models.Person;
+
+import java.util.List;
+import java.util.Map;
+
+abstract class FriendshipGraphImpl implements FriendshipGraph {
+    protected final ApiClient apiClient;
+    protected Multimap<Person, Person> result;
+    protected Map<Integer, List<Integer>> graph;
+    protected Map<Integer, Person> people;
+
+    abstract Person getPerson(Integer id) throws ClientException, ApiException;
+    abstract List<Integer> getFriends(Integer id) throws ClientException, ApiException;
+
+    public FriendshipGraphImpl(ApiClient apiClient) {
+        this.apiClient = apiClient;
+        this.result = TreeMultimap.create(Person.comparator, Person.comparator);
+    }
+
+    @Override
+    public void findHiddenFriends(Integer seed, int depth, int width) throws ClientException, ApiException{
+        graph = getFriendsGraphRecursion(depth, seed, width);
+    }
+
+    @Override
+    public Multimap<Person, Person> getResult() {
+        return result;
+    }
+
+    protected Map<Integer, List<Integer>> getFriendsGraphRecursion(int depth, int width, Integer id)
+            throws ClientException, ApiException {
+        if (depth == 0 || apiClient.isUserNotValid(id)) {
+            return graph;
+        }
+        if (graph.keySet().size() % 40 == 0) {
+            updateResult();
+        }
+        List<Integer> friends = getFriends(id);
+        graph.put(id, friends);
+        for (Integer friend : friends) {
+            if (Math.random() < depth * width / (double) friends.size()) {
+                getFriendsGraphRecursion(depth - 1, width, friend);
+            }
+        }
+        return graph;
+    }
+
+    @SneakyThrows
+    protected void updateResult() {
+        for (Integer hiddenId : graph.keySet()) {
+            for (Integer hidId : graph.get(hiddenId)) {
+                if (graph.get(hidId) == null)
+                    continue;
+                if (graph.get(hidId).contains(hiddenId))
+                    continue;
+                if (!graph.get(hiddenId).contains(hidId))
+                    continue;
+
+                Person hiddenPerson = getPerson(hiddenId);
+                Person hidPerson = getPerson(hidId);
+                result.put(hidPerson, hiddenPerson);
+            }
+        }
+    }
+}
