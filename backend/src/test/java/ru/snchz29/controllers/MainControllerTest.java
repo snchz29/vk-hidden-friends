b099268ceda11fd9ba4b41242f4d8fd4fc425d9c
snchz29 <sasha4888.terehov@gmail.com>
2021-08-25 22:19:40 +0300

    add login and password values

--- backend/src/test/java/ru/snchz29/controllers/MainControllerTest.java
+++ backend/src/test/java/ru/snchz29/controllers/MainControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.concurrent.TimeUnit;
@@ -15,11 +16,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MainControllerTest {
-
     @Autowired
     private MainController controller;
     private static WebDriver driver;
     public static LoginPage loginPage;
+    // Should be in vkAuth.properties
+    @Value("${test.login}")
+    private String login;
+    // Should be in vkAuth.properties
+    @Value("${test.password}")
+    private String password;
+
 
     static {
         WebDriverManager.chromedriver().setup();
@@ -32,7 +39,7 @@ class MainControllerTest {
     @SneakyThrows
     @Test
     void index() {
-        String responseString = controller.index();
+        String responseString = controller.index(0, "");
         assertThat(responseString).contains("isLoggedIn");
         String loginLink = getLoginLink();
         assertThat(loginLink).matches("https://oauth\\.vk\\.com/authorize\\?client_id=[0-9]*&display=popup&redirect_uri=http://localhost:8080/login&scope=friends,groups,photos&response_type=code&v=5\\.120");
@@ -40,7 +47,7 @@ class MainControllerTest {
 
     @SneakyThrows
     String getLoginLink() {
-        String responseString = controller.index();
+        String responseString = controller.index(0, "");
         JSONObject response = new JSONObject(responseString);
         if (!response.getBoolean("isLoggedIn"))
             return response.getString("loginLink");
@@ -51,9 +58,9 @@ class MainControllerTest {
     @Test
     void login() {
         driver.get(getLoginLink());
-        loginPage.inputLogin("login");
-        loginPage.inputPassword("password");
+        loginPage.inputLogin(login);
+        loginPage.inputPassword(password);
         loginPage.clickLoginBtn();
-
+        driver.quit();
     }
 }
