7f2a7a607257767c862aa24bac247e6fe3173b2c
snchz29 <sasha4888.terehov@gmail.com>
2021-08-25 20:13:07 +0300

    add tests

---
+++ backend/src/test/java/ru/snchz29/controllers/LoginPage.java
@@ -0,0 +1,36 @@
+package ru.snchz29.controllers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//*[@id=\"login_submit\"]/div/div/input[6]")
+    private WebElement loginField;
+
+    @FindBy(xpath = "//*[@id=\"login_submit\"]/div/div/input[7]")
+    private WebElement passwordField;
+
+    @FindBy(xpath = "//*[contains(@id, 'install_allow')]")
+    private WebElement loginBtn;
+
+    public void inputLogin(String login) {
+        loginField.sendKeys(login);
+    }
+
+    public void inputPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginBtn(){
+        loginBtn.click();
+    }
+}
---
+++ backend/src/test/java/ru/snchz29/controllers/MainControllerTest.java
@@ -0,0 +1,59 @@
+package ru.snchz29.controllers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.SneakyThrows;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MainControllerTest {
+
+    @Autowired
+    private MainController controller;
+    private static WebDriver driver;
+    public static LoginPage loginPage;
+
+    static {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        loginPage = new LoginPage(driver);
+    }
+
+    @SneakyThrows
+    @Test
+    void index() {
+        String responseString = controller.index();
+        assertThat(responseString).contains("isLoggedIn");
+        String loginLink = getLoginLink();
+        assertThat(loginLink).matches("https://oauth\\.vk\\.com/authorize\\?client_id=[0-9]*&display=popup&redirect_uri=http://localhost:8080/login&scope=friends,groups,photos&response_type=code&v=5\\.120");
+    }
+
+    @SneakyThrows
+    String getLoginLink() {
+        String responseString = controller.index();
+        JSONObject response = new JSONObject(responseString);
+        if (!response.getBoolean("isLoggedIn"))
+            return response.getString("loginLink");
+        else
+            return null;
+    }
+
+    @Test
+    void login() {
+        driver.get(getLoginLink());
+        loginPage.inputLogin("login");
+        loginPage.inputPassword("password");
+        loginPage.clickLoginBtn();
+
+    }
+}
