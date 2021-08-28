73fba0d701195caf890e64a78bffde39f2b1ef20
snchz29 <sasha4888.terehov@gmail.com>
2021-08-28 12:22:47 +0300

    delete some comment

--- backend/src/main/java/ru/snchz29/config/Config.java
+++ backend/src/main/java/ru/snchz29/config/Config.java
@@ -1,27 +1,7 @@
 package ru.snchz29.config;
 
-import com.vk.api.sdk.client.actors.UserActor;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.snchz29.services.ApiClient;
-import ru.snchz29.services.AuthProcessor;
 
 @Configuration
 public class Config {
-//    private final ApplicationContext context;
-
-//    public Config(ApplicationContext context) {
-//        this.context = context;
-//    }
-
-//    @Bean
-//    public ApiClient apiClient() {
-//        return new ApiClient() {
-//            @Override
-//            protected UserActor getUserActor() {
-//                return context.getBean(AuthProcessor.class).getUserActor();
-//            }
-//        };
-//    }
 }
